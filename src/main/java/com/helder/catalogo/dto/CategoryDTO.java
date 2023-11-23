86bf3bf4068da3c87ffa65d789bfbb0f92acb58f
heldersantos764 <heldersantos764@gmail.com>
2023-11-23 17:28:31 -0300

    DTO

---
+++ src/main/java/com/helder/catalogo/dto/CategoryDTO.java
@@ -0,0 +1,47 @@
+package com.helder.catalogo.dto;
+
+import java.io.Serializable;
+
+import com.helder.catalogo.entities.Category;
+
+public class CategoryDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	
+	public CategoryDTO() {
+		
+	}
+
+	public CategoryDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public CategoryDTO(Category entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
